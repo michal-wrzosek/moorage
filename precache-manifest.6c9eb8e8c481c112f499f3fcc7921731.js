ae950a04179fa8773836cca03ec6509419be0390
Michał Wrzosek <michal@wrzosek.pl>
2019-08-08 20:36:54 +0200

    Updates

---
+++ precache-manifest.6c9eb8e8c481c112f499f3fcc7921731.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d367e30061812b2f4c6cd60a086cb6e",
+    "url": "/moorage/index.html"
+  },
+  {
+    "revision": "b50b76dc2828b81dbba9",
+    "url": "/moorage/static/js/2.6143ffd4.chunk.js"
+  },
+  {
+    "revision": "ca5679d6a3308eb749e2",
+    "url": "/moorage/static/js/main.1a0a9106.chunk.js"
+  },
+  {
+    "revision": "48d709a15553d45cbd59",
+    "url": "/moorage/static/js/runtime~main.10ba9950.js"
+  }
+]);
